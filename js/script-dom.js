df2effe4975cb004f2dc1efa2015a7ddb4355906
David Viuche <jviucheroja@uniminuto.edu.co>
2020-04-14 18:44:41 -0500

    Buscar giph implementado

--- js/script-dom.js
+++ js/script-dom.js
@@ -33,6 +33,47 @@ class ModificarDOM {
             document.getElementById('estilos').href = "css/theme-dark.css";
             document.getElementById('logo').src = "img/gifOF_logo_dark.png";
         });
+
+        document.getElementById('texto-buscar').addEventListener('click',()=>{
+            document.getElementById('buscar').style.backgroundColor = '#F7C9F3';
+            let caja = document.getElementById('caja-sugeridos');
+            (caja.style.display == "flex") ? caja.style.display = "none" : caja.style.display = 'flex';
+        });
+
+        document.getElementById('texto-buscar').addEventListener('focusout',()=>{
+            document.getElementById('buscar').style.backgroundColor = '#e6e6e6';
+            let caja = document.getElementById('caja-sugeridos');
+            (caja.style.display == "flex") ? caja.style.display = "none" : caja.style.display = 'flex';
+        });
+
+        document.getElementById('buscar').addEventListener('click',()=>{
+            let texto = document.getElementById('texto-buscar').value;
+            if(texto){
+                document.getElementById('section-giph').innerHTML = "";
+                this.peticion(texto);
+            }
+        });
+    }
+
+    peticion(url){
+        let giphy = 'http://api.giphy.com/v1/gifs/search';
+        let key = 'p8v3HTqAOrj6dkDFYpjtOOyhSsJRLp6j';
+    
+        fetch(giphy+"?api_key="+key+"&q="+url)
+        .then((data)=>{
+            return data.json()
+        })
+        .then((json)=>{
+            json.data.map((el)=>{
+                let url = el.images.downsized_large.url;
+                let img = document.createElement('img');
+                img.src = url;
+                document.getElementById('section-giph').appendChild(img);
+            });
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
     }
 }
 
