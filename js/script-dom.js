d5acd24614efef2b3f9155ab13c41f3a79ef1f7f
David Viuche <jviucheroja@uniminuto.edu.co>
2020-04-20 12:36:42 -0500

    Funcionalidad de sugerencias de giph agregada

--- js/script-dom.js
+++ js/script-dom.js
@@ -1,113 +1,177 @@
-class ModificarDOM {
-    constructor() {
-        this.agregarEventListeners();
+let btnElegirTema = document.getElementById('btn-elegir-tema');
+let btnElegirTema2 = document.getElementById('btn-elegir-tema2');
+let opLight = document.getElementById('op-light');
+let opDark = document.getElementById('op-dark');
+let buscarContainer = document.getElementById('buscar-container');
+let cajaOpcionesTema = document.getElementById('opciones-tema');
+let linkCss = document.getElementById('estilos');
+let logo = document.getElementById('logo');
+let btnBuscar = document.getElementById('btn-buscar');
+let cajaSugeridos = document.getElementById('caja-sugeridos');
+let textoBusqueda = document.getElementById('texto-buscar');
+
+
+cajaOpcionesTema.addEventListener('mouseleave', () => {
+    cajaOpcionesTema.style.display = "none";
+});
+
+btnElegirTema.addEventListener('click', () => {
+    (cajaOpcionesTema.style.display == "flex") ? cajaOpcionesTema.style.display = "none" : cajaOpcionesTema.style.display = 'flex';
+});
+
+btnElegirTema2.addEventListener('click', () => {
+    (cajaOpcionesTema.style.display == "flex") ? cajaOpcionesTema.style.display = "none" : cajaOpcionesTema.style.display = 'flex';
+});
+
+opLight.addEventListener('click', () => {
+    linkCss.href = "css/theme-light.css";
+    logo.src = "img/gifOF_logo.png";
+});
+
+opDark.addEventListener('click', () => {
+    linkCss.href = "css/theme-dark.css";
+    logo.src = "img/gifOF_logo_dark.png";
+});
+
+buscarContainer.addEventListener('mouseleave', () => {
+    textoBusqueda.addEventListener('focusout', cerrarCajaSugeridos);
+    cajaSugeridos.style.display = 'none';
+});
+
+cajaSugeridos.addEventListener('mouseover', () => {
+    textoBusqueda.removeEventListener('focusout', cerrarCajaSugeridos);
+    cajaSugeridos.style.display = 'flex';
+});
+
+textoBusqueda.addEventListener('focus', () => {
+    btnBuscar.style.backgroundColor = '#F7C9F3';
+});
+
+textoBusqueda.addEventListener('input', () => {
+    cajaSugeridos.style.display = 'flex';
+    let texto = textoBusqueda.value;
+    if (texto)
+        peticionSugerencias(texto);
+
+});
+
+textoBusqueda.addEventListener('focusout', cerrarCajaSugeridos);
+
+btnBuscar.addEventListener('click', () => {
+    let texto = textoBusqueda.value;
+    if (texto) {
+        peticionBusqueda(texto);
     }
+});
 
-    agregarEventListeners() {
-        document.getElementById('btn-elegir-tema').addEventListener('click', () => {
-            let caja = document.getElementById('opciones-tema');
-            (caja.style.display == "flex") ? caja.style.display = "none" : caja.style.display = 'flex';
-        });
-
-        document.getElementById('btn-elegir-tema2').addEventListener('click', () => {
-            let caja = document.getElementById('opciones-tema');
-            (caja.style.display == "flex") ? caja.style.display = "none" : caja.style.display = 'flex';
-        });
-
-        document.getElementById('op-light').addEventListener('click', () => {
-            document.getElementById('estilos').href = "css/theme-light.css";
-            document.getElementById('logo').src = "img/gifOF_logo.png";
-        });
-
-        document.getElementById('op-dark').addEventListener('click', () => {
-            document.getElementById('estilos').href = "css/theme-dark.css";
-            document.getElementById('logo').src = "img/gifOF_logo_dark.png";
-        });
-
-        document.getElementById('buscar-container').addEventListener('mouseleave', () => {
-            document.getElementById('buscar').style.backgroundColor = '#F7C9F3';
-            let caja = document.getElementById('caja-sugeridos');
-            caja.style.display = 'none';
-            // (caja.style.display == "flex") ? caja.style.display = "none" : caja.style.display = 'flex';
-        });
-
-        document.getElementById('texto-buscar').addEventListener('input', () => {
-            document.getElementById('buscar').style.backgroundColor = '#e6e6e6';
-            let caja = document.getElementById('caja-sugeridos');
-            caja.style.display = 'flex';
-            let texto = document.getElementById('texto-buscar').value;
-            if (texto)
-                this.peticionSugerencias(texto);
-
-        });
-
-        document.getElementById('texto-buscar').addEventListener('blur', () => {
-            document.getElementById('buscar').style.backgroundColor = '#e6e6e6';
-            let caja = document.getElementById('caja-sugeridos');
-            caja.style.display = 'none';
-
-        });
-
-        document.getElementById('buscar').addEventListener('click', () => {
-            let texto = document.getElementById('texto-buscar').value;
-            if (texto) {
-                this.peticionBusqueda(texto);
-            }
-        });
-    }
-
-    peticionSugerencias(texto) {
-        let giphy = 'http://api.giphy.com/v1/gifs/search/tags';
-        let key = 'p8v3HTqAOrj6dkDFYpjtOOyhSsJRLp6j';
-
-        fetch(giphy + "?api_key=" + key + "&q=" + `'${texto}'`)
-            .then((data) => {
-                return data.json()
-            })
-            .then((json) => {
-                if (json.data.length) {
-                    document.getElementById('caja-sugeridos').innerHTML = "";
-
-                    for (let index = 0; index < json.data.length; index++) {
-                        let sug = document.createElement('button');
-                        sug.classList.add('btn-sugeridos');
-                        sug.id = `op-sugeridos-${index+1}`;
-                        sug.innerHTML = json.data[index].name;
-                        document.getElementById('caja-sugeridos').appendChild(sug);
-
-                        let sugb = document.getElementById(`op-sugeridos-${index+1}`);
-                        //sugb.addEventListener('click',this.peticionBusqueda(sugb.innerText));
-                        if(index==2)
-                            break;
-                    }
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
-
-    peticionBusqueda(url) {
-        let giphy = 'http://api.giphy.com/v1/gifs/search';
-        let key = 'p8v3HTqAOrj6dkDFYpjtOOyhSsJRLp6j';
-        console.log(giphy + "?api_key=" + key + "&q=" + url+"&limit="+10);
-        fetch(giphy + "?api_key=" + key + "&q=" + url)
-            .then((data) => {
-                return data.json()
-            })
-            .then((json) => {
-                document.getElementById('section-giph').innerHTML = "";
-                json.data.map((el) => {
-                    let url = el.images.downsized_large.url;
-                    let img = document.createElement('img');
-                    img.src = url;
-                    document.getElementById('section-giph').appendChild(img);
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
+function cerrarCajaSugeridos() {
+    btnBuscar.style.backgroundColor = '#e6e6e6';
+    cajaSugeridos.style.display = 'none';
 }
 
-window.onload = () => new ModificarDOM();
+function peticionSugerencias(texto) {
+    let giphy = 'http://api.giphy.com/v1/gifs/search/tags';
+    let key = 'p8v3HTqAOrj6dkDFYpjtOOyhSsJRLp6j';
+
+    fetch(giphy + "?api_key=" + key + "&q=" + `'${texto}'`)
+        .then((data) => {
+            return data.json()
+        })
+        .then((json) => {
+            if (json.data.length) {
+                cajaSugeridos.innerHTML = "";
+
+                for (let index = 0; index < json.data.length; index++) {
+                    let sug = document.createElement('button');
+                    sug.classList.add('btn-sugeridos');
+                    sug.id = `op-sugeridos-${index + 1}`;
+                    sug.innerHTML = json.data[index].name;
+                    sug.addEventListener('click', () => {
+                        peticionBusqueda(sug.innerText);
+                    });
+                    cajaSugeridos.appendChild(sug);
+
+                    if (index == 2)
+                        break;
+                }
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+function peticionBusqueda(url) {
+    let giphy = 'http://api.giphy.com/v1/gifs/search';
+    let key = 'p8v3HTqAOrj6dkDFYpjtOOyhSsJRLp6j';
+    // console.log(giphy + "?api_key=" + key + "&q=" + url + "&limit=" + 10);
+    fetch(giphy + "?api_key=" + key + "&q=" + url)
+        .then((data) => {
+            return data.json()
+        })
+        .then((json) => {
+            document.getElementById('container-sugerencia-giph').innerHTML = "";
+            json.data.map((el) => {
+                let url = el.images.original.url;
+                let img = document.createElement('img');
+                img.src = url;
+                document.getElementById('container-sugerencia-giph').appendChild(img);
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+function peticionGiphSugerencia(id) {
+
+    let giphy = 'https://api.giphy.com/v1/gifs/random';
+    let key = 'p8v3HTqAOrj6dkDFYpjtOOyhSsJRLp6j';
+    let rating = '&rating=g';
+
+    fetch(giphy + "?api_key=" + key + rating)
+        .then((data) => {
+            return data.json();
+        })
+        .then((json) => {
+            if (json.data) {
+                let cajaT = document.createElement('div');
+                cajaT.classList.add('caja-tendencia');
+                cajaT.id = `tendencia${id}`;
+                let cajaBorde = document.createElement('div');
+                cajaBorde.classList.add('borde-ventana');
+                let titulo = document.createElement('label');
+                titulo.innerText = json.data.title;
+                let imgcerrar = document.createElement('img');
+                imgcerrar.src = "img/close.svg";
+                imgcerrar.alt = "boton-cerrar";
+                imgcerrar.id = `cerrar${id}`;
+                let imgGiph = document.createElement('img');
+                imgGiph.classList.add('giph');
+                imgGiph.src = json.data.images.downsized_large.url;
+                let buttonVerMas = document.createElement('button');
+                buttonVerMas.innerText = 'Ver más...';
+                cajaBorde.appendChild(titulo);
+                cajaBorde.appendChild(imgcerrar);
+                cajaT.appendChild(cajaBorde);
+                cajaT.appendChild(imgGiph);
+                cajaT.appendChild(buttonVerMas);
+                imgcerrar.addEventListener('click', () => {
+                    let elemento = document.getElementById(`tendencia${id}`);
+                    document.getElementById('container-sugerencia-giph').removeChild(elemento);
+                    peticionGiphSugerencia(id);
+                });
+                let containerSugerencias = document.getElementById('container-sugerencia-giph');
+                containerSugerencias.appendChild(cajaT);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+
+for (let i = 0; i < 4; i++) {
+    peticionGiphSugerencia(i);
+}
+
