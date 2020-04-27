cbba6c002607b9f3803b534a4253b513cfc82e49
David Viuche <jviucheroja@uniminuto.edu.co>
2020-04-26 21:31:33 -0500

    primer intento crear gif

--- js/script-misgifos.js
+++ js/script-misgifos.js
@@ -15,6 +15,15 @@ const containerCrearGif = document.getElementById('container-crear-giph');
 const containerPrecaptura = document.getElementById('container-precaptura');
 const video = document.getElementById('video');
 const btnCerrarVentana = document.getElementById('btn-cerrar-captura');
+const btnCapturarImg = document.getElementById('btn-capturar-img');
+const btnCapturar = document.getElementById('btn-capturar');
+const imgCamara = document.getElementById('icon-camara');
+const imgGrabando = document.getElementById('icon-grabando');
+const btnDetenerImg = document.getElementById('btn-detener-img');
+const btnDetener = document.getElementById('btn-detener');
+const containerBtnCaptura = document.getElementById('container-botones-captura');
+const containerBtnGrabacion = document.getElementById('botones-grabacion');
+const imgGIFCreado = document.getElementById('gif-creado');
 
 cajaOpcionesTema.addEventListener('mouseleave', () => {
     cajaOpcionesTema.style.display = "none";
@@ -68,6 +77,62 @@ btnComenzar.addEventListener('click', () => {
     getStreamAndRecord();
 });
 
+btnCerrarVentana.addEventListener('click', () => {
+    window.location.reload();
+});
+
+btnCapturarImg.addEventListener('click', iniciarGrabacion);
+btnCapturar.addEventListener('click', iniciarGrabacion);
+btnDetenerImg.addEventListener('click', detenerGrabacion);
+btnDetener.addEventListener('click', detenerGrabacion);
+
+function iniciarGrabacion() {
+    containerBtnCaptura.style.display = 'none';
+    containerBtnGrabacion.style.display = 'flex';
+    recorderVideo.startRecording();
+    recorderGif.startRecording();
+}
+
+// function looper() {
+//    console.log(recorder.state);
+//     if(recorder.state === 'stopped') return; // ignore+stop
+//     setTimeout(looper, 1000); // update after every 3-seconds
+// }
+
+function detenerGrabacion() {   
+    recorderVideo.stopRecording(function () {
+        let video2 = document.getElementById('video2');
+        let blobVideo = recorderVideo.getBlob();
+        let videoURL = window.URL.createObjectURL(blobVideo);
+        video.style.display = 'none';
+        localStream.stop();
+        video2.src = videoURL;
+        video2.play();
+        video2.controls = true;
+    });
+    recorderGif.stopRecording(function () {
+        let blobGif = recorderGif.getBlob();
+        let gifURL = window.URL.createObjectURL(blobGif);
+        imgGIFCreado.src = gifURL;
+    });
+}
+
+let localStream;
+let recorderVideo;
+let configVideo = {
+    mimeType: 'video/webm',
+    bitsPerSecond: 128000
+}
+
+let recorderGif;
+let confiGif = {
+    type: 'gif',
+    frameRate: 1,
+    quality: 10,
+    width: 360,
+    hidden: 240
+}
+
 function getStreamAndRecord() {
     navigator.mediaDevices.getUserMedia({
         audio: false,
@@ -76,10 +141,13 @@ function getStreamAndRecord() {
             height: { max: 341 }
         }
     })
-    .then(function (stream) {
-        video.srcObject = stream;
-        video.play()
-    })
+        .then(function (stream) {
+            localStream = stream;
+            recorderVideo = RecordRTC(localStream, configVideo);
+            recorderGif = RecordRTC(localStream, confiGif);
+            video.srcObject = stream;
+            video.play()
+        })
 }
 function cargarDatosIniciales() {
 
@@ -90,10 +158,12 @@ function cargarDatosIniciales() {
             linkCss.href = "css/theme-light.css";
             logo.src = "img/gifOF_logo.png";
             localStorage.setItem('theme', 'light');
+            imgCamara.src = 'img/camera.svg';
         } else {
             linkCss.href = "css/theme-dark.css";
             logo.src = "img/gifOF_logo_dark.png";
             localStorage.setItem('theme', 'dark');
+            imgCamara.src = 'img/camera_light.svg';
         }
     }
 }
